11e1db77b1eadcefa7e857b75bf0fcc16373ff7a
Rahuldeb5 <rahuldeb.usa@gmail.com>
2024-01-19 22:51:53 -0500

    Adding templates and organizing repo

--- usaco_guide/bronze/simulation/shellGame.cpp
+++ usaco_guide/bronze/simulation/shellGame.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    // freopen(".in", "r", stdin);
+    // freopen(".out", "w", stdout);
+
+
+
+    return 0;
+}
+
+/*
+Rahuldeb5
+¯\_(ツ)_/¯
+*/
