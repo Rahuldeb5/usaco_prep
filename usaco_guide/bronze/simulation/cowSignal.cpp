65b74941ed4c032b79ce075be067ef54abd4c44b
Rahuldeb5 <rahuldeb.usa@gmail.com>
2024-01-21 16:21:24 -0500

    Solved Cow Signal

--- usaco_guide/bronze/simulation/cowSignal.cpp
+++ usaco_guide/bronze/simulation/cowSignal.cpp
@@ -3,10 +3,10 @@ typedef long long ll;
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
+    // ios::sync_with_stdio(0); cin.tie(0);
 
-    // freopen(".in", "r", stdin);
-    // freopen(".out", "w", stdout);
+    freopen("cowsignal.in", "r", stdin);
+    freopen("cowsignal.out", "w", stdout);
 
     int m, n, k; cin >> m >> n >> k;
 
